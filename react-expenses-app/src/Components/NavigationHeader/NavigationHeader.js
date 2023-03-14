95487806cb4f3b02695f2af021cffc1dbdc56f19
ravinder sian <rsian761@gmail.com>
2023-03-14 20:42:00 +0000

    get rid of login and register

--- react-expenses-app/src/Components/NavigationHeader/NavigationHeader.js
+++ react-expenses-app/src/Components/NavigationHeader/NavigationHeader.js
@@ -5,24 +5,6 @@ const NavigationHeader = () => {
   return (
     <nav className={`${classes["nav-box"]}`}>
       <h1>Budget Application</h1>
-      <button className={`${classes["btn-register"]}`}>Register</button>
-      <form name="loginForm" action="/performlogin" method="POST">
-        <div className={`${classes["login-inputs"]}`}>
-          <input
-            className={`${classes["login-input"]}`}
-            type="text"
-            name="username"
-            placeholder="Username"
-          ></input>
-          <input
-            className={`${classes["login-input"]}`}
-            type="password"
-            name="password"
-            placeholder="Password"
-          ></input>
-          <input name="submit-login" type="submit" value="submit" />
-        </div>
-      </form>
       <input
         className={`${classes["search-bar"]}`}
         type="search"
